2bf02a2ceb707faf15063d94a81555a3c92bc42a
Martin Vladimirov <martinvlad@MacBook-Pro-2.local>
2021-03-14 17:13:59 -0700

    added

--- src/mineField/Minefield.java
+++ src/mineField/Minefield.java
@@ -1,4 +1,21 @@
 package mineField;
 
-public class Minefield {
+import mvc.*;
+import java.awt.Color;
+
+public class Minefield extends Model {
+    private Color color = Color.GREEN;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public String toString() { return "stopLight.color = " + color; }
+
+    public void change() {
+        if (color.equals(Color.GREEN)) color = Color.YELLOW;
+        else if (color.equals(Color.YELLOW)) color = Color.RED;
+        else if (color.equals(Color.RED)) color = Color.GREEN;
+        changed(); // from Model, sets changed flag and fires changed event
+    }
 }
