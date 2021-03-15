c974578513186de0b0090e844c39a8fb7373014a
andre <andre.domingo@sjsu.edu>
2021-03-14 17:20:49 -0700

    fixed minefieldview

--- src/mineField/MinefieldView.java
+++ src/mineField/MinefieldView.java
@@ -1,25 +1,20 @@
 package mineField;
 
-
-import java.awt.*;
 import mvc.*;
+import java.awt.*;
 
-public class MinefieldView {
+public class MinefieldView extends View {
 
+    public MinefieldView(Minefield light) {
+        super(light);
+    }
 
-    public  MinefieldView extends View {
-
-        public MinefieldView(MineField light) {
-            super(light);
-        }
-
-        public void paintComponent(Graphics gc) {
-            super.paintComponent(gc);
-            Stoplight light = (Stoplight)model;
-            Color oldColor = gc.getColor();
-            gc.setColor(light.getColor());
-            gc.fillOval(100, 100, 20, 20);
-            gc.setColor(oldColor);
-        }
+    public void paintComponent(Graphics gc) {
+        super.paintComponent(gc);
+        Minefield light = (Minefield)model;
+        Color oldColor = gc.getColor();
+        gc.setColor(light.getColor());
+        gc.fillOval(100, 100, 20, 20);
+        gc.setColor(oldColor);
     }
 }
