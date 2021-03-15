9a7d648d4f0f3fffa7ea9acba15b5516842031da
Martin Vladimirov <martinvlad@MacBook-Pro-2.local>
2021-03-14 17:25:49 -0700

    added

--- src/mineField/Minefield.java
+++ src/mineField/Minefield.java
@@ -5,12 +5,20 @@ import java.awt.Color;
 
 public class Minefield extends Model {
     private Color color = Color.GREEN;
+    public int x = 0;
+    public int y = 0;
 
     public Color getColor() {
         return color;
     }
 
     public String toString() { return "stopLight.color = " + color; }
+
+    public String getLocation(int x, int y){
+        this.x = x;
+        this.y = y;
+        return "The location is" + x + y;
+    }
 
     public void change() {
         if (color.equals(Color.GREEN)) color = Color.YELLOW;
