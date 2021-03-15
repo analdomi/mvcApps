6ffdc3d055ddf1e221b0607585b1daed3d40dabb
Martin Vladimirov <martinvlad@MacBook-Pro-2.local>
2021-03-14 17:11:08 -0700

    added

--- src/mineField/MinefieldView.java
+++ src/mineField/MinefieldView.java
@@ -1,8 +1,25 @@
 package mineField;
 
+
+import java.awt.*;
+import mvc.*;
+
 public class MinefieldView {
-    public MinefieldView(){
 
+
+    public  MinefieldView extends View {
+
+        public MinefieldView(MineField light) {
+            super(light);
+        }
+
+        public void paintComponent(Graphics gc) {
+            super.paintComponent(gc);
+            Stoplight light = (Stoplight)model;
+            Color oldColor = gc.getColor();
+            gc.setColor(light.getColor());
+            gc.fillOval(100, 100, 20, 20);
+            gc.setColor(oldColor);
+        }
     }
-
 }
