53be3ce6782a58d2ce715ab4daf95f9d7bc16369
andre <andre.domingo@sjsu.edu>
2021-03-15 15:11:27 -0700

    Added to Cell class

--- src/mineField/Cell.java
+++ src/mineField/Cell.java
@@ -13,4 +13,28 @@ public class Cell implements Serializable{
         mined = Math.random() < minedChance;
         traversed = false;
     }
+
+    public void setNeighboringMines(int mines) {
+        neighboringMines = mines;
+    }
+
+    public int getNeighboringMines() {
+        return neighboringMines;
+    }
+
+    public void mine() {
+        mined = true;
+    }
+
+    public boolean isMined() {
+        return mined;
+    }
+
+    public boolean isTraversed() {
+        return traversed;
+    }
+
+    public void traverse() {
+        traversed = true;
+    }
 }
