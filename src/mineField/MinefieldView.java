931366418e1a02cc1810f66eb5a8ca8b6e28e52e
Martin Vladimirov <martinvlad@MacBook-Pro-2.local>
2021-03-21 20:49:51 -0700

    added

--- src/mineField/MinefieldView.java
+++ src/mineField/MinefieldView.java
@@ -5,8 +5,8 @@ import java.awt.*;
 
 public class MinefieldView extends View {
 
-    private static final int RECT_X = 10;
-    private static final int RECT_Y = 10;
+    private static final int RECT_X = 12;
+    private static final int RECT_Y = 12;
     private static final int RECT_WIDTH = 10;
     private static final int RECT_HEIGHT = 10;
     public MinefieldView(Minefield light) {
@@ -24,6 +24,10 @@ public class MinefieldView extends View {
         for(int i =0; i< minefield.WORLD_SIZE ; i++){
             for(int j =0; j < minefield.WORLD_SIZE; j++){
                 gc.drawRect(RECT_X * i, RECT_Y * j, RECT_WIDTH , RECT_HEIGHT);
+                gc.drawString("?", RECT_X,RECT_Y);
+                gc.setColor(Color.GRAY);
+                gc.fillRect(RECT_X * i, RECT_Y * j, RECT_WIDTH , RECT_HEIGHT);
+
             }
 
         }
