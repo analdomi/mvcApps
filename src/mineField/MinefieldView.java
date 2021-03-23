ac9e264913a20850497b4353228382e03434ac13
Koosha-Kimelman <kooshakimelman@yahoo.com>
2021-03-22 20:27:43 -0700

    Modified colors, and spaced cell text

--- src/mineField/MinefieldView.java
+++ src/mineField/MinefieldView.java
@@ -11,28 +11,36 @@ public class MinefieldView extends View {
     private static final int RECT_HEIGHT = 20;
     public MinefieldView(Minefield minefield) {
         super(minefield);
+        setBackground(Color.gray);
     }
 
     public void paintComponent(Graphics gc) {
         super.paintComponent(gc);
         Minefield minefield = (Minefield)model;
-        minefield.getGrid();
 
         Color oldColor = gc.getColor();
-        System.out.print(minefield);
-        //gc.setColor(light.getColor());
-        for(int i =0; i< minefield.WORLD_SIZE ; i++){
-            for(int j =0; j < minefield.WORLD_SIZE; j++){
+        for(int i =0; i< Minefield.WORLD_SIZE ; i++){
+            for(int j =0; j < Minefield.WORLD_SIZE; j++){
                 Grid grid = minefield.getGrid();
                 Cell cell = grid.getCell(i, j);
                 if(cell.isTraversed()) {
-                    gc.setColor(Color.white);
-                    gc.drawString("" + cell.getNeighboringMines(), RECT_X * i, RECT_Y * (j + 1));
+                    if (i == minefield.getX() && j == minefield.getY()) {
+                        gc.setColor(Color.cyan);
+                    } else {
+                        gc.setColor(Color.white);
+                    }
+                    gc.drawString("" + cell.getNeighboringMines(), (RECT_X * i) + (RECT_X / 3),(RECT_Y * (j + 1)) - (RECT_Y / 4));
+                                    // Conversion to String
                 }
                 else{
                     gc.setColor(Color.black);
-                    gc.drawString("?", RECT_X * i,RECT_Y * (j + 1));
+                    gc.drawString("?", (RECT_X * i) + (RECT_X / 3),(RECT_Y * (j + 1)) - (RECT_Y / 4));
                 }
+
+                if (i == Minefield.WORLD_SIZE - 1 && j == Minefield.WORLD_SIZE - 1) {
+                    gc.setColor(Color.green);
+                }
+
                 gc.drawRect(RECT_X * i, RECT_Y * j, RECT_WIDTH , RECT_HEIGHT);
             }
 
